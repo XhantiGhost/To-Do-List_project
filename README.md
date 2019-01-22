3ffe60b19226d175999f0e0c422917b2ad331e4b
XhantiGhost <xhantijacob@gmail.com>
2019-01-22 12:26:14 +0200

    we back

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # To-Do-List_project
+
+# Create a to-do App
+
+## ****Brief:
+# **You are going to use PHP and MySQL to create an app (on a webpage) that you can use to keep track of various tasks. This app must be able to do the following:
+
+• Add tasks to the list
+
+• Delete tasks from the list
+
+• Edit tasks
+
+• Add due dates to tasks
+
+• Sort tasks alphabetically
+
+ 
+
+ **In addition to that functionality you should do the following while making your app:
+
+• All repeated code must be in functions
+
+• You must have a seperate function file
+
+• Use CSS to style your application
+
+
+The google form link is here.
+
+Have fun!
--- index.php
+++ index.php
@@ -4,10 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/todolist/style/style.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe" rel="stylesheet">
     <title>To Do List</title>
 </head>
 <body>
+<div class="header">
+<h1 style ="font-family: 'ZCOOL KuaiLe', cursive; ">MY IMPORTANT TO DO LIST!!!</h1>
+</div>
+	<form method="post" action="index.php" class="input_form">
+		<input type="text" name="task" class="task_input">
+		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
+	</form>
     
 </body>
 </html>
