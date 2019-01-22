a298bf266ad783a5df6861ea35326b1a4e89e3d1
XhantiGhost <xhantijacob@gmail.com>
2019-01-22 15:53:55 +0200

    we back

--- index.php
+++ index.php
@@ -4,18 +4,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="/style/style.css">
     <link href="https://fonts.googleapis.com/css?family=ZCOOL+KuaiLe" rel="stylesheet">
     <title>To Do List</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 <div class="header">
-<h1 style ="font-family: 'ZCOOL KuaiLe', cursive; ">MY IMPORTANT TO DO LIST!!!</h1>
+<h1 style ="font-family: 'ZCOOL KuaiLe', cursive; text-alian: center; ">MY IMPORTANT TO DO LIST!!!</h1>
+<h2 style ="font-family: 'ZCOOL KuaiLe', cursive; "> Delete Done TASK'S </h2>
 </div>
 	<form method="post" action="index.php" class="input_form">
 		<input type="text" name="task" class="task_input">
 		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
 	</form>
-    
 </body>
 </html>
